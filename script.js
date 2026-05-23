b12dd669afcff0e72933f57460ae3f92a80422cf
taiwosogbesan146-stack <taiwosogbesan146@gmail.com>
2026-05-23 23:47:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+const filters = document.querySelectorAll('.filter');
+const cards = document.querySelectorAll('.card');
+const themeBtn = document.querySelector('.theme-btn');
+
+filters.forEach(button => {
+  button.addEventListener('click', () => {
+
+    filters.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+
+    const text = button.textContent;
+
+    cards.forEach(card => {
+
+      if (text === 'All') {
+        card.style.display = 'block';
+      }
+
+      else if (text === 'Active') {
+        if (card.classList.contains('active-card')) {
+          card.style.display = 'block';
+        } else {
+          card.style.display = 'none';
+        }
+      }
+
+      else if (text === 'Inactive') {
+        if (card.classList.contains('inactive-card')) {
+          card.style.display = 'block';
+        } else {
+          card.style.display = 'none';
+        }
+      }
+    });
+  });
+});
+
+const removeButtons = document.querySelectorAll('.remove-btn');
+
+removeButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    button.closest('.card').remove();
+  });
+});
+
+themeBtn.addEventListener('click', () => {
+
+  document.body.classList.toggle('light-mode');
+
+  if (document.body.classList.contains('light-mode')) {
+    themeBtn.innerHTML = '🌙';
+  } else {
+    themeBtn.innerHTML = '☀';
+  }
+});
